3a38db89950f6ae0f2fd682d97326db00a392669
python <thomas.win@gmail.com>
2019-11-08 16:35:41 +0100

    add import function

---
+++ functionshifumi.py
@@ -0,0 +1,11 @@
+
+
+def computer_random():
+    import random
+    liste = [ 1, 2, 3] 
+    comp = random.choice(liste)
+    print ("Computer choose :",comp)
+    return comp
+
+
+
--- shifumi.py
+++ shifumi.py
@@ -10,21 +10,21 @@ print("=====================================")
 name=input("Hello, first enter your name: ")
 print("=====================================")
 
-import random
-liste = [ 1, 2, 3] #Computer choices
 scoreU=0 #User score
 scoreC=0 #Computer score
+from functionshifumi import * 
 
 
 
-try:
-   while scoreU != 3 or scoreC != 3:
-         print(name,",tap:\n 1 to play Stone,          2 to play Scissor,            3 to play Paper")  
+try: #let's introduce all cases.
+   while scoreU != 3 and scoreC != 3:#loop condition
+         print(name,",tap:\n 1 to play Stone,   2 to play Scissor,    3 to play Paper") # short tuto 
          user=int(input("Choose!"))
-         comp= random.choice(liste)
+         comp = computer_random()
+            
          
 
-         if user==3 and comp==1:
+         if user==3 and comp==1:#user win
             print("Paper beat Stone!")
             scoreU += 1
             print ("Bravo! Your score:", scoreU,"-Computer: ",scoreC)
@@ -43,11 +43,11 @@ try:
             print ("------------------------------------------------")
 
          if user==comp:
-            print("DRAW!")
+            print("DRAW!") #egality
             print ( "Your score:", scoreU,"-Computer: ",scoreC)              
             print ("------------------------------------------------")
 
-         if user==1 and comp==3:
+         if user==1 and comp==3: #computer win
             print("Paper beat Stone!")
             scoreC += 1
             print ("Too Bad! Your score:", scoreU,"-Computer: ",scoreC)
@@ -67,22 +67,25 @@ try:
 
 
 
-         if scoreU == 3 or scoreC == 3:
+         if scoreU == 3 or scoreC == 3: #condition that engage end game
                print("============================================")
                text = input("Continue ? Y/N :") #avoid breaking if user input too fast
-               if text== "Y":
+         
+               if text== "Y":#score réinitialisation
                   scoreU=0
                   scoreC=0
-               if text == "N":
+               if text == "N":#end game
                   print:("=========")
                   print("Game Over")
                   print("==========")
                   break
                if text not in ("Y","N"):
                   print("invalid command")
+                  
 
-except ValueError:
-       print("Invalid command")
+except: 
+  pass
+      
                
       
 
